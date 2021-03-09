121375fd7d53413b49541ba18fd84b73d7c4e2fe
ericfp87 <52512077+ericfp87@users.noreply.github.com>
2021-03-09 12:22:13 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+window = Tk()
+window.title("My first GUI program")
+window.minsize(width=500, height=300)
+
+#Label
+
+my_label = Label(text="I Am a Label", font=("Arial", 24, "bold"))
+my_label.pack()
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+#Button
+
+def button_clicked():
+    my_label.config(text=input.get())
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+#Entry
+
+input = Entry(width=10)
+input.pack()
+print(input.get())
+
+
+
+
+window.wait_window()
