106349953f126c59ab8746050a3ef8e2dc61a411
ericfp87 <52512077+ericfp87@users.noreply.github.com>
2021-03-09 12:39:13 -0300

    final

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ my_label.pack()
 
 my_label["text"] = "New Text"
 my_label.config(text="New Text")
+my_label.grid(column=0, row=0)
 
 #Button
 
@@ -18,13 +19,16 @@ def button_clicked():
     my_label.config(text=input.get())
 
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
 
+new_button = Button(text="New Button", command=button_clicked)
+new_button.grid(column=2, row=0)
 #Entry
 
 input = Entry(width=10)
-input.pack()
+
 print(input.get())
+input.grid(column=3, row=2)
 
 
 
